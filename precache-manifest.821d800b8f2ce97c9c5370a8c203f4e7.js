3e73ebe640c9ad9158efc136e8d1e260d7bdb50c
Aashish kandel <kandelaashish0001@gmail.com>
2020-04-16 20:02:16 -0400

    Updates

---
+++ precache-manifest.821d800b8f2ce97c9c5370a8c203f4e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19ef19145c67e5af91dd1c7f5d79202",
+    "url": "/task-remainder/index.html"
+  },
+  {
+    "revision": "4cc43ae46f3d8d9fc342",
+    "url": "/task-remainder/static/js/2.b32fbfce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/task-remainder/static/js/2.b32fbfce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2600de437f8f56f8fabf",
+    "url": "/task-remainder/static/js/main.80bd4dad.chunk.js"
+  },
+  {
+    "revision": "708648932b966e8df8cd",
+    "url": "/task-remainder/static/js/runtime-main.899b17fd.js"
+  }
+]);
